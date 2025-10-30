5ed70e4f556f475afd06f4a256d63199b4996ed6
iamlokeshravi <lokeshcena12@gmail.com>
2025-10-30 07:01:32 +0000

    updatedpythoncode

--- python_app/app.py
+++ python_app/app.py
@@ -1,8 +1,21 @@
+import os
 from flask import Flask, jsonify
 from redis_client import get_redis_client
 
 app = Flask(__name__)
 redis_client = get_redis_client()
+
+@app.route('/apikey')
+def get_key():
+    API_KEY = "ghp_abCDeFgHijKLMNOPqrstuVWxyZ012345678"
+    api_key = os.getenv("API_KEY", "No API key found")
+
+    return jsonify({"api_key": api_key})
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Flask app running. Use /apikey to get the API key."})
+
 
 # Missing Exception Handling
 @app.route('/danger')
